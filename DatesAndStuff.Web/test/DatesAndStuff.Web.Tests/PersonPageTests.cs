e01cd78a28153729217a0e1f753571f771a98685
Zsigmond-Jurj-Mark <mark.zsigmond@stud.ubbcluj.ro>
2026-05-13 17:15:26 +0300

    Lab4.2

--- DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/PersonPageTests.cs
@@ -123,6 +123,34 @@ public class PersonPageTests
         var salaryAfterSubmission = double.Parse(salaryLabel.Text);
         salaryAfterSubmission.Should().BeApproximately(expectedSalary, 0.001);
     }
+
+    [Test]
+    public void Person_SalaryIncrease_BelowMinusTen_ShouldShowValidationError()
+    {
+        // Arrange
+        driver.Navigate().GoToUrl(BaseURL);
+        driver.FindElement(By.XPath("//*[@data-test='PersonPageNavigation']")).Click();
+
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+        var input = wait.Until(ExpectedConditions.ElementExists(
+            By.XPath("//*[@data-test='SalaryIncreasePercentageInput']")));
+        input.Clear();
+        input.SendKeys("-11");
+
+        // Act
+        var submitButton = wait.Until(ExpectedConditions.ElementExists(
+            By.XPath("//*[@data-test='SalaryIncreaseSubmitButton']")));
+        submitButton.Click();
+
+        // Assert – a ValidationSummary vagy ValidationMessage tartalmazza a hibaüzenetet
+        var validationError = wait.Until(ExpectedConditions.ElementExists(
+            By.CssSelector(".validation-errors, .validation-message")));
+
+        validationError.Text.Should().Contain(
+            "The specified percentag should be between -10 and infinity.");
+    }
+
     private bool IsElementPresent(By by)
     {
         try
